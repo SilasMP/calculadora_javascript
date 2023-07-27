9b556bbd6c67d8e376b69bf7aef7166c77e0c60a
Silas de Melo <silas_melo@yahoo.com.br>
2023-07-27 13:18:57 -0300

    Atualizado o nome de alguns atributos e metodos

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,17 +4,17 @@ class CalcController {
         this._mainDisplayEl = document.querySelector('#display'); //display principal da calc
         this._topDisplayEl = document.querySelector('#data'); //display superior, memoria
         this._timeEl = document.querySelector('#hora');// display que mostra a hora       
-        this._firstField = []; //armazena temporariamente os numeros do display principal
-        this._othersFields = []; // memoria onde armazena os numeros do display superior para as operações
+        this._currentNumber = []; //armazena temporariamente os numeros do display principal
+        this._memoryNumber = []; // memoria onde armazena os numeros do display superior para as operações
         this._keyboardOn = true; // bloqueia o teclado caso exceda o numero de caracteres do display
         this.initialize(); // função que inicia as informações para os displays
         this.initButtonsEvents();// função que gera eventos aos botões
-        this.initKeyboard();//função que identifica as teclas pressionadas no teclado
+        this.initKeyboardEvents();//função que identifica as teclas pressionadas no teclado
     }
 
     initialize(){ 
         
-        this._topDisplayEl.innerHTML = (this._othersFields == undefined ? '':this._othersFields);
+        this._topDisplayEl.innerHTML = (this._memoryNumber == undefined ? '':this._memoryNumber);
         this._timeEl.innerHTML = new Date().toLocaleTimeString('pt-br',{'hour':'2-digit','minute':'2-digit'});
 
         //Atualizado a hora a cada segundo
@@ -38,7 +38,7 @@ class CalcController {
         })
     }
 
-    initKeyboard(){
+    initKeyboardEvents(){
 
         let keys = ['0','1','2','3','4','5','6','7',
             '8','9','.','=','+','-','*','/','%','ac','ce'];
@@ -62,21 +62,21 @@ class CalcController {
         if (isNaN(btnValue)==false || btnValue == '.'){//Condição caso o valor recebido seja um numero ou um ponto
             
             if (this._keyboardOn){//cond para bloquear a inclusão de novos numeros caso excedido os caracteres do display
-                if (this._othersFields.join().slice(-1) == '='){//cond caso o ultimo caracter da memoria seja o btn igual
+                if (this._memoryNumber.join().slice(-1) == '='){//cond caso o ultimo caracter da memoria seja o btn igual
 
-                    this.clear(this._firstField); 
-                    this.clear(this._othersFields);
-                    this.topDisplay = this._othersFields;                           
+                    this.clear(this._currentNumber); 
+                    this.clear(this._memoryNumber);
+                    this.topDisplay = this._memoryNumber;                           
                     
                 }
-                this._firstField.push(btnValue)
+                this._currentNumber.push(btnValue)
                 
-                if (this._firstField.length > 1){//cond para concatenar mais de um numero
+                if (this._currentNumber.length > 1){//cond para concatenar mais de um numero
                     
-                    this._firstField = [this._firstField.join('')];
+                    this._currentNumber = [this._currentNumber.join('')];
                         
                 }
-                this.mainDisplay = this._firstField;
+                this.mainDisplay = this._currentNumber;
 
             }else{
                 return false
@@ -85,38 +85,37 @@ class CalcController {
         }else if (btnValue == 'ac' || btnValue == 'ce'){//cond para limpar a tela conforme valor recebido
             
             if (btnValue == 'ac'){
-                this.clear(this._firstField); 
-                this.clear(this._othersFields);                
+                this.clear(this._currentNumber); 
+                this.clear(this._memoryNumber);                
                 
             }else {
-                this.clear(this._firstField);                
+                this.clear(this._currentNumber);                
             }
 
             this.mainDisplay = '0'; 
-            this.topDisplay = this._othersFields;
+            this.topDisplay = this._memoryNumber;
             this._keyboardOn = true;
 
         }else if (btnValue == '='){// cond caso o valor recebido seja o sinal de igual
             this._keyboardOn = true;
             this.operations(btnValue);
-            this._othersFields = [this._othersFields + this._firstField + btnValue];
-            this.topDisplay = this._othersFields;
+            this._memoryNumber = [this._memoryNumber + this._currentNumber + btnValue];
+            this.topDisplay = this._memoryNumber;
             
         }else{ // condição caso valor recebido seja um sinal de operação          
             this._keyboardOn = true;
-            if(this._othersFields.length == 0){
-                this._othersFields = [this._firstField+btnValue];
-                this.topDisplay = this._othersFields;
-                this._firstField = [];                
+            if(this._memoryNumber.length == 0){
+                this._memoryNumber = [this._currentNumber+btnValue];
+                this.topDisplay = this._memoryNumber;
+                this._currentNumber = [];                
 
-            }else if(this._firstField.length == 0){
-
-                this._othersFields = [this._othersFields.join().slice(0,-1)+btnValue]
-                this.topDisplay = this._othersFields;
+            }else if(this._currentNumber.length == 0){
+                this._memoryNumber = [this._memoryNumber.join().slice(0,-1)+btnValue]
+                this.topDisplay = this._memoryNumber;
                 
             }else {                
                 this.operations(btnValue);
-                this.topDisplay = this._othersFields;                
+                this.topDisplay = this._memoryNumber;                
             }
         }
     }
@@ -130,25 +129,25 @@ class CalcController {
 
         try{
             if (symbol == "="){
-                this.mainDisplay = eval(this._othersFields+this._firstField);
-                this._othersFields = [eval(this._othersFields+this._firstField)];
-                this._firstField = [];            
+                this.mainDisplay = eval(this._memoryNumber+this._currentNumber);
+                this._memoryNumber = [eval(this._memoryNumber+this._currentNumber)];
+                this._currentNumber = [];            
 
             }else if(symbol == '%') { 
-                this._firstField = [eval(this._firstField / 100)];
-                this.mainDisplay = this._firstField;
+                this._currentNumber = [eval(this._currentNumber / 100)];
+                this.mainDisplay = this._currentNumber;
                 
             }else{
-                this.mainDisplay = eval(this._othersFields+this._firstField);
-                this._othersFields = [eval(this._othersFields+this._firstField) + symbol]
-                this._firstField = []
-                this.topDisplay = this._othersFields;            
+                this.mainDisplay = eval(this._memoryNumber+this._currentNumber);
+                this._memoryNumber = [eval(this._memoryNumber+this._currentNumber) + symbol]
+                this._currentNumber = []
+                this.topDisplay = this._memoryNumber;            
             }
         } catch{//caso de algum erro na operação
             this.mainDisplay = '0';
             this.topDisplay = '';
-            this._othersFields = [];
-            this._firstField = [];
+            this._memoryNumber = [];
+            this._currentNumber = [];
         }     
     }
 
